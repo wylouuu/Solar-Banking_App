f671201ac23ece447e301fd331f3edb97574a406
wylouuu <lojokowilly@gmail.com>
2024-08-25 00:38:17 +0700

    implement sign-in screen layout

---
+++ components/CustomInput.tsx
@@ -0,0 +1,49 @@
+import {
+	FormControl,
+	FormField,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { authFormSchema } from '@/lib/utils';
+import { Control, FieldPath } from 'react-hook-form';
+import { z } from 'zod';
+
+interface CustomInputProps {
+	control: Control<z.infer<typeof authFormSchema>>;
+	name: FieldPath<z.infer<typeof authFormSchema>>;
+	label: string;
+	placeholder: string;
+}
+
+const CustomInput = ({
+	control,
+	name,
+	label,
+	placeholder,
+}: CustomInputProps) => {
+	return (
+		<FormField
+			control={control}
+			name={name}
+			render={({ field }) => (
+				<div className='form-item'>
+					<FormLabel className='form-label'>{label}</FormLabel>
+					<div className='flex w-full flex-col'>
+						<FormControl>
+							<Input
+								placeholder={placeholder}
+								className='input-class'
+								type={name === 'password' ? name : 'text'}
+								{...field}
+							/>
+						</FormControl>
+						<FormMessage className='form-message mt-2' />
+					</div>
+				</div>
+			)}
+		/>
+	);
+};
+
+export default CustomInput;
