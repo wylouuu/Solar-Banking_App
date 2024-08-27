ad51f1bdadda4a47c7b3dfb6598275e815a64630
wylouuu <lojokowilly@gmail.com>
2024-08-28 00:32:20 +0700

    implement categories on rightsidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -49,7 +49,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
 			<RightSidebar
 				user={loggedIn}
-				transactions={accounts?.transaction}
+				transactions={account?.transactions}
 				banks={accountsData?.slice(0, 2)}
 			/>
 		</section>
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import BankCard from './BankCard';
+import { countTransactionCategories } from '@/lib/utils';
+import { Category } from './Category';
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+	const categories: CategoryCount[] = countTransactionCategories(transactions);
+
+	// console.log(transactions);
+
 	return (
 		<aside className='right-sidebar'>
 			<section className='flex flex-col pb-8'>
@@ -53,6 +59,16 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
 						)}
 					</div>
 				)}
+
+				<div className='mt-10 flex flex-1 flex-col gap-6'>
+					<h2 className='header-2'>Top Categories</h2>
+
+					<div className='space-y-5'>
+						{categories.map((category, index) => (
+							<Category key={category.name} category={category} />
+						))}
+					</div>
+				</div>
 			</section>
 		</aside>
 	);
