0e93d2666026f342386a667968a9dc5f3aa280e3
wylouuu <lojokowilly@gmail.com>
2024-08-27 16:09:48 +0700

    implement full plaid and dwolla registration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 	const loggedIn = await getLoggedInUser();
+	const accounts = await getAccounts({
+		userId: loggedIn.$id,
+	});
+
+	if (!accounts) return;
+
+	const accountsData = accounts?.data;
+	const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+	const account = await getAccount({ appwriteItemId });
+
+	console.log(accountsData, account);
+
 	return (
 		<section className='home'>
 			<div className='home-content'>
@@ -12,14 +26,14 @@ const Home = async () => {
 					<HeaderBox
 						type='greeting'
 						title='Welcome'
-						user={loggedIn?.name || 'Guest'}
+						user={loggedIn?.firstName || 'Guest'}
 						subtext='Access and manage your account and transaction efficiently'
 					/>
 
 					<TotalBalanceBox
-						account={[]}
-						totalBanks={1}
-						totalCurrentBalance={1250.35}
+						accounts={accountsData}
+						totalBanks={accounts?.totalBanks}
+						totalCurrentBalance={accounts?.totalCurrentBalance}
 					/>
 				</header>
 				RECENT TRANSACTIONS
@@ -27,8 +41,8 @@ const Home = async () => {
 
 			<RightSidebar
 				user={loggedIn}
-				transactions={[]}
-				banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+				transactions={accounts?.transaction}
+				banks={accountsData?.slice(0, 2)}
 			/>
 		</section>
 	);
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
 	return (
 		<footer className='footer'>
 			<div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
-				<p className='text-xl font-bold text-gray-700'>{user?.name[0]}</p>
+				<p className='text-xl font-bold text-gray-700'>{user?.firstName[0]}</p>
 			</div>
 			<div
 				className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
